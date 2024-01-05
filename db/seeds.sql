83dd39b5c3483e42626a85002e6c54474a2c07f3
jabrams513 <145389973+jabrams513@users.noreply.github.com>
2024-01-04 22:29:28 -0500

    updated README

--- db/seeds.sql
+++ db/seeds.sql
@@ -22,7 +22,7 @@ VALUES ("Sales Lead", 100000, 1),
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("John", "Doe", 1, null),
        ("Mike", "Chan", 2, 1),
-       ("Ashley", "Roriguez", 3, null),
+       ("Ashley", "Rodriguez", 3, null),
        ("Kevin", "Tupik", 4, 3),
        ("Kunal", "Singh", 5, null),
        ("Aliah", "Brown", 6, 5),
